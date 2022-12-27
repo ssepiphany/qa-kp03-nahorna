629ed648d40373eebbe7c08c6477f097007c9598
Sofiia Nahorna <snagorna03@gmail.com>
2022-12-27 19:09:10 +0200

    lab1 finished

--- sem_1_lab_1/filesystem.py
+++ sem_1_lab_1/filesystem.py
@@ -5,60 +5,46 @@ from typing import List
 class FileSystem:
     def __init__(self) -> None:
         self.paths = collections.defaultdict(list)
+        self.DIR_MAX_ELEMS = 5
         self.logBinFileContents = collections.defaultdict(str)
         self.MAX_BUF_FILE_SIZE = 5
         self.buffContents = collections.defaultdict(list)
 
     def mkdir(self, path : str):
-        added = False
-        dirs = path.split("/")  
-        for i in range(1, len(dirs)):
-            cur = "/".join(dirs[:i])
-            if cur not in self.paths or dirs[i] not in self.paths[cur]:
-                added = True
-                self.paths[cur].append(dirs[i])
+        added = self.add_elements_to_directory(path)
         if (added == False):
             raise Exception("Directory alredy exists.")
 
     def createFile(self, path : str, content : str): 
-        created = False
-        dirs = path.split("/")
-
-        for i in range(1, len(dirs)):
-            cur = "/".join(dirs[:i])
-            if cur not in self.paths or dirs[i] not in self.paths[cur]:
-                created = True
-                self.paths[cur].append(dirs[i])
-
+        added = self.add_elements_to_directory(path)
         self.logBinFileContents[path] = content
-        if (created == False):
+        if (added == False):
             raise Exception("File already exists.")
 
     def createBufferFile(self, path : str): 
-        created = False
-        dirs = path.split("/")
-
-        for i in range(1, len(dirs)):
-            cur = "/".join(dirs[:i])
-            if cur not in self.paths or dirs[i] not in self.paths[cur]:
-                created = True
-                self.paths[cur].append(dirs[i])
-
+        added = self.add_elements_to_directory(path)
         self.buffContents[path].append(object())
-        if (created == False):
+        if (added == False):
             raise Exception("File already exists.")
 
     def ls(self, path : str) -> List[str]:  
         return self.paths[path]
 
     def moveDirectory(self, oldPath : str, newPath: str):
-        if (oldPath in self.logBinFileContents):
-            self.createFile(newPath)
-            self.logBinFileContents[newPath] = self.logBinFileContents[oldPath]
-            del self.logBinFileContents[oldPath]
-            self.deleteFile(oldPath)
+        dirs = oldPath.split("/")
+        pathToDir = "/".join(dirs[0:-1])
+        if (dirs[-1] in self.paths[pathToDir]):
+            content = self.paths[oldPath]
+            self.paths[pathToDir].remove(dirs[-1])
+            del self.paths[oldPath]
+            self.mkdir(newPath)
+            self.paths[newPath] += content
+            self.delete_empty_directory_path(pathToDir)
+            self.moveSubdirectories(oldPath, newPath)
+            self.moveInnerBinaryAndLogTextFiles(oldPath, newPath)
+            self.moveInnerBufferFiles(oldPath, newPath)
         else:
-            raise Exception("File doesn't exist.")
+            raise Exception("Directory doesn't exist.")
 
     def moveFile(self, oldPath : str, newPath: str):
         if (oldPath in self.logBinFileContents):
@@ -92,8 +78,7 @@ class FileSystem:
     def pushElement(self, path : str, el: object): 
         if path not in self.buffContents:
             raise Exception("File doesn't exist.")
-        elif len(self.buffContents[path]) == self.MAX_BUF_FILE_SIZE: #queue.Queue(self.buffContents[path]).full()):
-            # return False
+        elif len(self.buffContents[path]) == self.MAX_BUF_FILE_SIZE:
             raise Exception("Cannot push new element to file. Maximum size [" + str(self.MAX_BUF_FILE_SIZE) + "] was reached.")
 
         self.buffContents[path].append(el)
@@ -114,11 +99,8 @@ class FileSystem:
                 del self.logBinFileContents[path] 
             elif (path in self.buffContents):
                 del self.buffContents[path]  
-
             self.paths[pathToFile].remove(dirs[-1])
-
-            if len(self.paths[pathToFile]) == 0:
-                del self.paths[pathToFile]
+            self.delete_empty_directory_path(pathToFile)
 
         else:
             raise Exception("File doesn't exist.")
@@ -131,9 +113,7 @@ class FileSystem:
             self.deleteDirectoryFiles(path)
             self.deleteDirectorySubdirectories(path)
             self.paths[pathToDir].remove(dirs[-1])
-
-            if len(self.paths[pathToDir]) == 0:
-                del self.paths[pathToDir]
+            self.delete_empty_directory_path(pathToDir)
         else:
             raise Exception("Directory doesn't exist.")
 
@@ -149,3 +129,43 @@ class FileSystem:
         for k in list(self.paths.keys()):
             if path in k:
                 del self.paths[k] 
+
+    def moveSubdirectories(self, oldPath : str, newPath : str):
+        temp = dict(self.paths)
+        for path in temp:
+            if str(path).startswith(oldPath):
+                common_part = path[len(oldPath)+1:]
+                self.paths[newPath + "/" + common_part] = self.paths[path]
+                del self.paths[path]
+
+    def moveInnerBinaryAndLogTextFiles(self, oldPath : str, newPath : str):
+        temp = dict(self.logBinFileContents)
+        for file_path in temp:
+            if str(file_path).startswith(oldPath):
+                common_part = file_path[len(oldPath)+1:]
+                self.logBinFileContents[newPath + "/" + common_part] = self.logBinFileContents[file_path]
+                del self.logBinFileContents[file_path]
+
+    def moveInnerBufferFiles(self, oldPath : str, newPath : str):
+        temp = dict(self.buffContents)
+        for file_path in temp:
+            if str(file_path).startswith(oldPath):
+                common_part = file_path[len(oldPath)+1:] 
+                self.buffContents[newPath + "/" + common_part] = self.buffContents[file_path]
+                del self.buffContents[file_path]
+
+    def add_elements_to_directory(self, path : str) -> bool:
+        added = False
+        dirs = path.split("/")  
+        for i in range(1, len(dirs)):
+            cur = "/".join(dirs[:i])
+            if cur not in self.paths or dirs[i] not in self.paths[cur]:
+                if len(self.paths[cur]) == self.DIR_MAX_ELEMS:
+                    raise Exception("Directory " + cur + " reached limit of elements [" + str(self.DIR_MAX_ELEMS) +"].")
+                added = True
+                self.paths[cur].append(dirs[i])
+        return added
+
+    def delete_empty_directory_path(self, dirPath):
+        if len(self.paths[dirPath]) == 0:
+                del self.paths[dirPath]
