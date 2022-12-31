9be82db64405601d31182a289d21cb1032f84cdb
Sofiia Nahorna <snagorna03@gmail.com>
2022-12-31 08:46:29 +0200

    prototype of the system created

---
+++ sem_1_lab_2/application.py
@@ -0,0 +1,75 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/directory/create', methods=['POST'])
+def mkdir():
+    pass
+
+@app.route('/binaryfile/create', methods=['POST'])
+def create_bin_file():
+    pass
+
+@app.route('/logtextfile/create', methods=['POST'])
+def create_log_text_file():
+    pass
+
+@app.route('/bufferfile/create', methods=['POST'])
+def create_buffer_file():
+    pass
+
+@app.route('/directory/list', methods=['GET'])
+def list():
+    pass
+
+@app.route('/directory/delete', methods=['DELETE'])
+def delete_directory():
+    pass
+
+@app.route('/binaryfile/delete', methods=['DELETE'])
+def delete_binary_file():
+    pass
+
+@app.route('/logtextfile/delete', methods=['DELETE'])
+def delete_log_text_file():
+    pass
+
+@app.route('/bufferfile/delete', methods=['DELETE'])
+def delete_buffer_file():
+    pass
+
+@app.route('/directory/move', methods=['PUT'])
+def move_directory():
+    pass
+
+@app.route('/binaryfile/move', methods=['PUT'])
+def move_bin_file():
+    pass
+
+@app.route('/logtextfile/move', methods=['PUT'])
+def move_log_text_file():
+    pass
+
+@app.route('/bufferfile/move', methods=['PUT'])
+def move_buffer_file():
+    pass
+
+@app.route('/binaryfile/read', methods=['GET'])
+def read_bin_file():
+    pass
+
+@app.route('/logtextfile/read', methods=['GET'])
+def read_log_text_file():
+    pass
+
+@app.route('/logtextfile/append', methods=['PUT'])
+def append_line_to_log_text_file():
+    pass
+
+@app.route('/bufferfile/push', methods=['PUT'])
+def push_element_to_buffer_file():
+    pass
+
+@app.route('/bufferfile/consume', methods=['PUT'])
+def consume_element_from_buffer_file():
+    pass
---
+++ sem_1_lab_2/filesystem.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class FileSystem:
+    def __init__(self) -> None:
+        pass
+
+    def mkdir(self, path : str):
+        pass
+
+    def createFile(self, path : str, content : str): 
+        pass
+
+    def createBufferFile(self, path : str): 
+        pass
+
+    def ls(self, path : str) -> List[str]:  
+        pass
+
+    def moveDirectory(self, oldPath : str, newPath: str):
+        pass
+
+    def moveFile(self, oldPath : str, newPath: str):
+        pass
+
+    def moveBufferFile(self, oldPath : str, newPath : str):
+        pass
+
+    def readFile(self, path : str) -> str:
+        pass
+
+    def appendLineToFile(self, path : str, line : str):
+        pass
+
+    def pushElement(self, path : str, el: object): 
+        pass
+
+    def consumeElement(self, path : str):
+        pass
+
+    def deleteFile(self, path : str):
+        pass
+
+    def deleteDirectory(self, path : str):
+        pass
